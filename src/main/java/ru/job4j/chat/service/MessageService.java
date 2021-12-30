9b6c4a78cdfe338674414f9f170e423cb74368d8
ignatown <ignatow.n@mail.ru>
2021-12-31 02:30:38 +0300

    time of create for message

--- src/main/java/ru/job4j/chat/service/MessageService.java
+++ src/main/java/ru/job4j/chat/service/MessageService.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Service;
 import ru.job4j.chat.model.Message;
 import ru.job4j.chat.repository.MessageRepository;
 
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
 @Service
 public class MessageService {
     private final MessageRepository messageRepository;
@@ -21,6 +24,7 @@ public class MessageService {
     }
 
     public Message saveMessage(Message message) {
+        message.setCreated(Timestamp.valueOf(LocalDateTime.now()));
         return messageRepository.save(message);
     }
 
